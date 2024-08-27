c5ccf259b0e60d4a6a2df3e096d37b1e1c85d0e5
Painkiller995 <fahdakramdaher@gmail.com>
2024-08-27 13:34:21 +0200

    update frontend to the latest version

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000, // This is the port which we will use in docker
-    // add the next lines if you're using windows and hot reload doesn't work
-    watch: {
-      usePolling: true,
-    },
-  },
-});
+})

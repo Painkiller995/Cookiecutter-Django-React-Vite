73847ede7410544022b63e4541987b40bafc7ec5
Painkiller995 <fahdakramdaher@gmail.com>
2024-08-27 13:45:03 +0200

    add server settings to the frontend

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000, // This is the port which we will use in docker
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
 })

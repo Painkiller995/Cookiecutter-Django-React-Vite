cecb6c896640b46d6685f3a9037ae5f3e0ba78ed
Painkiller995 <fahdakramdaher@gmail.com>
2025-07-23 17:31:24 +0200

    feat: add server configuration to Vite for improved development experience

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000, // This is the port which we will use in docker
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
 })
